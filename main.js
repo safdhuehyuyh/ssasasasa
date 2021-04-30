d194d9a3837fded118d1ffbdf010b7a158e23976
Samyak Bhoyar <78425159+safdhuehyuyh@users.noreply.github.com>
2021-04-30 15:17:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,66 +1,20 @@
-Webcam.set({
-    width:310,
-    height:300,
-    image_format : 'png',
-    png_quality:90
-  });
+function preload(){
 
-  camera = document.getElementById("camera");
-
-Webcam.attach('#camera');
-
-      
-function take_snapshot()
-{
-    Webcam.snap(function(data_uri) {
-        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
-    });
 }
-console.log('ml5 version:', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Pljt69ahJ/model.json', modelLoaded);
-
-function modelLoaded(){
-    console.log("Model Loaded!");
+function setup(){
+    canvas = createCanvas(640, 480);
+    canvas.position(110, 250);
+    video = createCapture(VIDEO);
+    video.hide();
+    tint_color = ""; 
 }
-
-function speak(){
-    var synth = window.speechSynthesis;
-    speak_data_1 = "The first prediction is " + prediction_1;
-    speak_data_2 = "And the second prediction is " + prediction_2;
-    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2)
-    synth.speak(utterThis);
+function draw(){
+    image(video, 0,0,640,480);
+    tint(tint_color);
 }
-function check(){
-    img = document.getElementById("captured_image");
-    classifier.classify(img, gotResult);
+function take_snapshot(){
+    save('student_name.png');
 }
-function gotResult(error, results) {
-    if (error) {
-        console.error(error);
-    } else {
-        console.log(results)
-        document.getElementById("result_emotion_name").innerHTML = results[0].label;
-        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
-        prediction_1 = results[0].label;
-        prediction_2 = results[1].label;
-        speak();
-        if (results[0].label == "best"){
-            document.getElementById("update_emoji").innerHTML = "&#128077;";
-        }
-        if (results[0].label == "victory"){
-            document.getElementById("update_emoji").innerHTML = "&#9996;";
-        }
-        if (results[0].label == "amazing"){
-            document.getElementById("update_emoji").innerHTML = "&#128076;";
-        }
-        if (results[1].label == "best"){
-            document.getElementById("update_emoji2").innerHTML = "&#128077;";
-        }
-        if (results[1].label == "victory"){
-            document.getElementById("update_emoji2").innerHTML = "&#9996;";
-        }
-        if (results[1].label == "amazing"){
-            document.getElementById("update_emoji2").innerHTML = "&#128076;";
-        }
-    }
+function filter_tint(){
+    tint_color = document.getElementById("color_name").value;
 }
